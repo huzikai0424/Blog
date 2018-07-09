78d04d5413fd814b3bdac82a0be97ccf6a2c8eed
huzikai0424 <hzk@cherryml.com>
2018-07-09 13:43:08 +0800

    修正时间格式化

--- server/routes/common.js
+++ server/routes/common.js
@@ -10,7 +10,7 @@ module.exports={
         let date = new Date(time)
         let year = date.getFullYear()
         let month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
-        let day = date.getDate()
+        let day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
         return `${year}年${month}月${day}日`
     },
     formatTimeToDay:function(time){
